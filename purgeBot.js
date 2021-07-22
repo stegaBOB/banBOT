e208d7b483dca22c9e25abbd4ab0bed65fa3e1bc
stegaBOB <sammyisaacharris@gmail.com>
2021-07-22 05:09:46 -0500

    Fixed async code stuff and finalized the bot

--- purgeBot.js
+++ purgeBot.js
@@ -1,3 +1,9 @@
+/**
+ * If you want to start up a purgeBOT on your own, add a .env file that contains
+ * `BOT_TOKEN="YOUR BOT TOKEN HERE"` or just replace the process.env.BOT_TOKEN in client.login
+ * with your token. Also, make sure to enable "SERVER MEMBERS INTENT" under the "Privileged Gateway Intents" 
+ * section in the Bot category on the Discord bot page.
+ */ 
 require('dotenv').config();
 const { Client, Intents } = require("discord.js");
 const intents = new Intents([
@@ -11,28 +17,40 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if(!message.guild) return;
-    if(!message.member.hasPermission("ADMINISTRATOR")) return;
-    else if(message.content.toLowerCase() === "%purgenorole"){
-        message.reply("Are you sure you want to kick all members without a role?\n"+
-        "Confirm with a thumb up or deny with a thumb down.")
-        .then(m => {
-            m.react('👍').then(r => {
-            m.react('👎');});
-            m.awaitReactions((reaction, user) => user.id == message.author.id && (reaction.emoji.name == '👍' || reaction.emoji.name == '👎'),
-            { max: 1, time: 15000 }).then(collected => {
-                if (collected.first().emoji.name == '👍') {
-                    message.channel.send('Kicking all members without a role...');
-                    purgeNoRole(message);
-                } else {
-                    message.reply('Operation canceled.');
-                } 
-            }).catch(() => {
-                message.channel.send('No reaction after 15 seconds, operation canceled');
-            });
-        });
+
+    //Moderator only commands:
+
+    if (message.member.hasPermission("ADMINISTRATOR") || message.member.roles.cache.find(r => r.name === "Moderator")) {
+        if(message.content.toLowerCase() === "%countpurge"){
+            countPurge(message);
+        } else if (message.content.toLowerCase() === "%countonlyusers" || message.content.toLowerCase() === "%countusers"){
+            countPurge(message, 3);
+        }
     }
-    else if(message.content.toLowerCase() === "%countpurge"){
-        countPurge(message);
+
+
+    //Admin only commands:
+
+    if(message.member.hasPermission("ADMINISTRATOR")){
+        if(message.content.toLowerCase() === "%purgenorole"){
+            message.reply("Are you sure you want to kick all members without a role?\n"+
+            "Confirm with a thumb up or deny with a thumb down.")
+            .then(m => {
+                m.react('👍').then(r => {
+                m.react('👎');});
+                m.awaitReactions((reaction, user) => user.id == message.author.id && (reaction.emoji.name == '👍' || reaction.emoji.name == '👎'),
+                { max: 1, time: 15000 }).then(collected => {
+                    if (collected.first().emoji.name == '👍') {
+                        message.channel.send('Kicking all members without a role...');
+                        purgeNoRole(message);
+                    } else {
+                        message.reply('Operation canceled.');
+                    } 
+                }).catch(() => {
+                    message.channel.send('No reaction after 15 seconds, operation canceled');
+                });
+            });
+        }
     }
 });
 
@@ -41,33 +59,35 @@ function purgeNoRole(message){
     message.guild.members.fetch()
     .then(members=>{
         let i = 0;
-        members.each(member=>{
+        members.each(async member=>{
             if(member.roles.cache.size < 2){
-                member.kick("Member has no roles. Authentication has not been completed.")
+               member.kick("Member has no roles. Authentication has not been completed.")
                 .catch(() => {
                     console.error;
                 });
                 i++;
             }
+        })
+        .then(r=>{
+            message.channel.send(`Kicked ${i} members.`);
+            console.log(`Kicked ${i} members.`);
         });
-        message.channel.send("Kicked " + i + " members.");
-        console.log("Kicked " + i + " members.");
     })
     .catch(console.error);
 }
 
-function countPurge(message){
-    console.log("COUNTING");
+function countPurge(message, lessThanRoles = 2){
+    console.log(`COUNTING MEMBERS WITH LESS THAN ${lessThanRoles} ROLE(S)`);
     message.guild.members.fetch()
     .then(members=>{
         let i = 0;
         members.each(member=>{
-            if(member.roles.cache.size < 2){
+            if(member.roles.cache.size < lessThanRoles){
                 i++;
             }
         });
-        console.log("There are " + i + " members without a role.");
-        message.channel.send("There are " + i + " members without a role.");
+        console.log(`There are ${i} members with less than ${lessThanRoles-1} role(s).`);
+        message.channel.send(`There are ${i} members with less than ${lessThanRoles-1} role(s).`);
     });
 }
 
