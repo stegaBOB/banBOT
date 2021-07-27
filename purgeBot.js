59b8481629074f1732476009f0b9e428b4d98c09
stegaBOB <41593264+stegaBOB@users.noreply.github.com>
2021-07-27 17:26:36 -0500

    Fixed purge issue and improved logging.

--- purgeBot.js
+++ purgeBot.js
@@ -38,18 +38,37 @@ client.on('message', message => {
 
     //Admin only commands:
 
-    if(message.member.hasPermission("ADMINISTRATOR")){
+    if(message.member.hasPermission("ADMINISTRATOR") || message.member.roles.cache.find(r => r.name === "Moderator")){
         if(message.content.toLowerCase() === "%purgenorole"){
             message.reply("Are you sure you want to kick all members without a role?\n"+
             "Confirm with a thumb up or deny with a thumb down.")
             .then(m => {
-                m.react('👍').then(r => {
-                m.react('👎');});
-                m.awaitReactions((reaction, user) => user.id == message.author.id && (reaction.emoji.name == '👍' || reaction.emoji.name == '👎'),
+                m.react('\U0001f44d').then(r => {
+                m.react('\U0001f44e');});
+                m.awaitReactions((reaction, user) => user.id == message.author.id && (reaction.emoji.name == '\U0001f44d' || reaction.emoji.name == '\U0001f44e'),
                 { max: 1, time: 15000 }).then(collected => {
-                    if (collected.first().emoji.name == '👍') {
+                    if (collected.first().emoji.name == '\U0001f44d') {
                         message.channel.send('Kicking all members without a role...');
                         purgeNoRole(message);
+                    } else {
+                        message.channel.send('Operation canceled.');
+                    } 
+                }).catch(() => {
+                    message.channel.send('No reaction after 15 seconds, operation canceled');
+                });
+            });
+        } else if(message.content.toLowerCase().startsWith("%testpurge")){
+	    let numRoles = parseInt(message.content.split(' ')[1]);
+            message.reply("Are you sure you want to test some stuff? Why am I asking?\n"+
+            "Confirm with a thumb up or deny with a thumb down.")
+            .then(m => {
+                m.react('\U0001f44d').then(r => {
+                m.react('\U0001f44e');});
+                m.awaitReactions((reaction, user) => user.id == message.author.id && (reaction.emoji.name == '\U0001f44d' || reaction.emoji.name == '\U0001f44e'),
+                { max: 1, time: 15000 }).then(collected => {
+                    if (collected.first().emoji.name == '\U0001f44d') {
+                        message.channel.send('Testing some stuff...');
+                        testPurge(message, numRoles);
                     } else {
                         message.channel.send('Operation canceled.');
                     } 
@@ -64,26 +83,64 @@ client.on('message', message => {
 function purgeNoRole(message){
     console.log("PURGING");
     message.guild.members.fetch()
-    .then(members=>{
+    .then(members => {
         const promises = [];
-        members.each(member=>{
+	    let i = 0;
+        members.each(member => {
             if(member.roles.cache.size < 2){
-                setTimout(()=>{
-                    promises.push(member.kick("Member has no roles. Authentication has not been completed."));
-                }, 1000);
+		        i++;
+                promises.push(member.kick("Member has no roles. Authentication has not been completed."));
             }
-        })
+        });
         Promise.all(promises)
-        .catch(() => {
-            console.error;
+        .then(results => {
+            console.log(`promises.length = ${promises.length}`);
+	        console.log(`i = ${i}`);
+            message.channel.send(`Kicked ${promises.length} members.`);
+            console.log(`Kicked ${promises.length} members.`);
         })
-        .then(results=>{
-            message.channel.send(`Kicked ${results.length} members.`);
-            console.log(`Kicked ${results.length} members.`);
+	    .catch(err => {
+            console.error(err);
+	        message.reply("I think there was an error kicking members. Blame Bob's poor coding.");
         });
     })
-    .catch(console.error);
+    .catch(err => {
+	    console.error(err);
+	    message.reply("I think there was an error fetching members. Blame Bob's poor coding.");
+    });
 }
+
+function testPurge(message, numRoles){
+    console.log("TEST PURGING");
+    message.guild.members.fetch()
+    .then(members => {
+        const promises = [];
+	    let i = 0;
+        members.each(member => {
+            if(member.roles.cache.size < numRoles){
+		        i++;
+		        promises.push(member.fetch());
+            }
+        });
+        Promise.all(promises)
+        .then(results=>{
+	        console.log(results);
+            console.log(`promises.length = ${promises.length}`);
+	        console.log(`i = ${i}`);
+            message.channel.send(`Successfully counted ${promises.length} members.`);
+            console.log(`Successfully counted ${promises.length} members.`);
+        })
+	    .catch(err => {
+            console.error(err);
+	        message.reply("I think there was an error counting members. Blame Bob's poor coding.");
+        });
+    })
+    .catch(err => {
+	    console.error(err);
+	    message.reply("I think there was an error fetching members. Blame Bob's poor coding.");
+    });
+}
+
 
 function countRoles(message, lessThanRoles = 1){
     console.log(`COUNTING MEMBERS WITH LESS THAN ${lessThanRoles} ROLE(S)`);
